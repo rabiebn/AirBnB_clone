11d507656df319279440ed6fdaa10b75c4486dfb
rabiebn <rabie.bnmr@gmail.com>
2023-08-08 14:47:41 +0100

    4: Updated __init__ to create BaseModel obj from dict

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    BaseModle Module
+BaseModle Module
 """
 
 from datetime import datetime
@@ -12,7 +12,7 @@ class BaseModel():
     BaseModel Class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializing instances.
 
@@ -21,15 +21,29 @@ class BaseModel():
         created_at (datetime): Date&Time when an instance is created.
         updated_at (datetime): Date&Time when an instance is last modified.
         """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        if kwargs is not None:
+            for k in kwargs.keys():
+                if k == "__class__":
+                    continue
+                elif k == "created_at":
+                    self.created_at = datetime.strptime(
+                        kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
+                elif k == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        kwargs[k], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[k] = kwargs[k]
 
     def __str__(self):
         """
         str
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
@@ -42,9 +56,10 @@ class BaseModel():
         """
         Returns a dictionary containing all __dict__ keys/values
         of the instance.
+        'created_at' and 'updated_at' are converted to string.
         """
         obj_dict = self.__dict__.copy()
         obj_dict["created_at"] = self.created_at.isoformat()
         obj_dict["updated_at"] = self.updated_at.isoformat()
-        obj_dict ["__class__"] = self.__class__.__name__
+        obj_dict["__class__"] = self.__class__.__name__
         return obj_dict
