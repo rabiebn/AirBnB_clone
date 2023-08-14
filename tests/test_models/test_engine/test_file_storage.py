563babe19bf4e03992be09f171ed43b16fbb77f4
abdelmalek <abdelmalek@gmail.com>
2023-08-14 01:35:58 -0700

    update

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -161,7 +161,7 @@ class Test_file_storage_methods(unittest.TestCase):
         self.assertIn("Review." + rv.id, objs)
 
     def test_reload_no_file(self):
-        with self.assertRaises(FileNotFoundError):
+        with self.assertRaises(FileNotFoundError, msg="JSON file not found"):
             models.storage.reload()
 
     def test_reload_with_arg(self):
